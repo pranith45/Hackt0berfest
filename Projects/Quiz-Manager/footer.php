5332faecdb426bcf88cda8ee6d81bc7f6b738d2d
Banseedhar Gondaliya <56584368+Banseedhar01@users.noreply.github.com>
2021-10-14 10:33:58 +0530

    Add Quiz-Manager

---
+++ Projects/Quiz-Manager/footer.php
@@ -0,0 +1,8 @@
+<div class="footer" style="color: white; height: 80px; margin-top:0px;padding-top:10px;background-color:#343a40!important;display:flex:justify-content:center;align-items:center;">
+	<div class="col-xl-4">
+		<center> <p class="Covid-info" style="margin-bottom: 0;font-weight: 500; padding:0"> Quiz System   </p></center>	
+	</div>
+	<div class="col-xl-4" style="padding:0px">
+		<center> <p > © Copyright HC, 2021. All rights reserved </p></center>
+	</div>
+</div>
