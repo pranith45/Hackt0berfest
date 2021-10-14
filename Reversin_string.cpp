1b320138a7c14d56c08eaeead96950d0b99b9c35
pranith <66859567+pranith45@users.noreply.github.com>
2021-10-14 23:50:15 +0530

    Create Reversin_string.cpp

---
+++ Reversin_string.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+    string str = "GeeksforGeeks";
+    int n=str.length();
+    //Temporary string to store the reverse
+    string rev;
+    for(int i=n-1;i>=0;i--)
+      rev.push_back(str[i]);
+   
+    cout<<rev<<endl;
+    return 0;
+}
